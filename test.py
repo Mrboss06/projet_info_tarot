72c198fd8350a90607a76189089ecadd78d0fee1
Lolol <loic.azavant@gmail.com>
2023-09-19 17:54:05 +0200

    debut

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
-joueurs =["1", "2", "3", "4"]
-annonces = ["petite", "garde", 'garde-sans', 'garde-contre']
-
-parties = []
-scores = []
-
-def recuperer_les_resultats_de_la_donne(index_du_preneur, index_annonce, nb_bouts, score_donne):
-    parties.append((index_du_preneur, index_annonce, nb_bouts, score_donne))
+import customtkinter as ctk
+import tkinter as tk
 
 
-def calculer_les_scores():
-    
+root = ctk.CTk()
+
+
+
+
+root.mainloop()
