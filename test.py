e69f9ee1331155bc03a8fff503ab9ec09e9ee822
Lolol <loic.azavant@gmail.com>
2023-09-19 17:50:20 +0200

    hello

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-a= 5
-b = 7
+joueurs =["1", "2", "3", "4"]
+annonces = ["petite", "garde", 'garde-sans', 'garde-contre']
+
+parties = []
+scores = []
+
+def recuperer_les_resultats_de_la_donne(index_du_preneur, index_annonce, nb_bouts, score_donne):
+    parties.append((index_du_preneur, index_annonce, nb_bouts, score_donne))
+
+
+def calculer_les_scores():
+    
