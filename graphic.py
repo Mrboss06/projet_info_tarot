a2d661f1b31662355b765379caf3c959f3d6b055
Mrboss06 <114099591+Mrboss06@users.noreply.github.com>
2024-01-21 01:11:33 +0100

    Update graphic.py

--- graphic.py
+++ graphic.py
@@ -2,52 +2,64 @@ import pygame
 import sys
 import graphic_choisir_lobby
 import graphic_choisir_pseudo
+import graphic_attente_dans_lobby
+import graphic_tour_de_jeu
+from joueur import Joueur
+from graphic_constant import SCREEN_HEIGHT, SCREEN_WIDTH, CARD_SIZE
+
 
 pygame.init()
 pygame.font.init()
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 800
 
 
-font = pygame.font.SysFont('Roboto', 50)
+class Window:
+    def __init__(self, joueur: Joueur) -> None:
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.font = pygame.font.SysFont('Roboto', 50)
+        
+        self.cardsImg = {}
+        for card in ["A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12", "A13", "A14", "A15", "A16", "A17", "A18", "A19", "A20", "A21", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "K1", "K2", "K3", "K4", "K5", "K6", "K7", "K8", "K9", "K10", "K11", "K12", "K13", "K14", "P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10", "P11", "P12", "P13", "P14", "T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9", "T10", "T11", "T12", "T13", "T14"]:
+            self.cardsImg[card] = pygame.image.load(f"assets/cards/{card}.png")
+            self.cardsImg[card] = pygame.transform.scale(self.cardsImg[card], CARD_SIZE)
+        
+        self.tab_choose_username = graphic_choisir_pseudo.TabChooseUsername(self.screen)
+        self.tab_select_lobby = graphic_choisir_lobby.TabSelectLobby(self.screen)
+        self.tab_waiting_in_lobby = graphic_attente_dans_lobby.WaitingInLobby(self.screen)
+        self.tab_tour_de_jeu = graphic_tour_de_jeu.TourDeJeu(self.screen, self.cardsImg)
+        self.menu = ''
+        self.joueur = joueur
 
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    def run(self):
+        running = True
+        while running:
+            
+            self.screen.fill((0,0,0))
+            
+            events = pygame.event.get()
+            
+            for event in events:
+                if event.type == pygame.QUIT:
+                    running = False
+            
+            mouse_pos = pygame.mouse.get_pos()
+            
+            #rect_with_mouse(pygame.Rect.collidepoint(rect, *mouse_pos))
+            #pygame.draw.rect(screen, (255,0,0), rect)
+            
+            if self.menu == 'username':
+                self.screen.blit(self.font.render("Ton pseudo", False, (255,255,255)), (50, 100))
+                self.tab_choose_username.choisir_pseudo(events)
+            elif self.menu == 'choisir_lobby':
+                self.tab_select_lobby.selectionner_lobby(events, mouse_pos)
+            elif self.menu == 'attente_dans_lobby':
+                self.tab_waiting_in_lobby.draw()
+            elif self.menu == 'tour_de_jeu':
+                self.tab_tour_de_jeu.update(events, mouse_pos, self.joueur.main)
+            else:
+                self.screen.blit(self.font.render("En attente", False, (255, 255, 255)), (60, 60))
+            
+            
+            pygame.display.update()
 
-graphic_choisir_pseudo.init(screen)
-graphic_choisir_lobby.init(screen)
-#graphic_choisir_lobby.init_lobby([(1, ("J1", "J2", "J3", "J7")), (2, ("J4", "J5")), (4, ("J6",)), (5, tuple())], [-1])
-
-menu = ''
-
-def run():
-    running = True
-    while running:
-        
-        screen.fill((0,0,0))
-        
-        ev = pygame.event.get()
-        
-        for event in ev:
-            if event.type == pygame.QUIT:
-                running = False
-        
-        mouse_pos = pygame.mouse.get_pos()
-        
-        #rect_with_mouse(pygame.Rect.collidepoint(rect, *mouse_pos))
-        #pygame.draw.rect(screen, (255,0,0), rect)
-        
-        if menu == 'username':
-            screen.blit(font.render("Ton pseudo", False, (255,255,255)), (50, 100))
-            graphic_choisir_pseudo.choisir_pseudo(ev)
-        elif menu == 'choisir_lobby':
-            graphic_choisir_lobby.selectionner_lobby(ev, mouse_pos)
-        elif menu == 'attente_dans_lobby':
-            screen.blit(font.render("En attente de joueurs", False, (255,)*3), (100, 100))
-        else:
-            screen.blit(font.render("En attente", False, (255, 255, 255)), (60, 60))
-        
-        
-        pygame.display.update()
-
-    pygame.quit()
+        pygame.quit()
